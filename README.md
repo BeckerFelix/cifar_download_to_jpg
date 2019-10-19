9b9fd633eef9f37a387f88916f756c04d7878197
MilkWithCoffee <milkwithcoffee@mailbox.org>
2019-10-19 17:40:01 +0200

    mostly code quality improvements

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # cifar_download_to_jpg
 
-This script gets the **Cifar10** dataset and exports each image as separate .jpg file. The files are stored according to given train/val ratio, where the folder name corresponds to the object class.  Really simple script which might hopefully help someone ;)
+This script gets the **Cifar10** dataset and exports each image as separate .jpg file. The files are stored according to given train/val ratio, where the folder name corresponds to the object class.  Really simple script.
 
-Data is gathered from cs.toronto.edu
+Data is pulled from cs.toronto.edu
 
-Use with python 2.7
+#Requirements:
+* Python 2.7
+* scipy 1.2.2
+* numpy
 
 ![sample output](https://raw.githubusercontent.com/MilkWithCoffee/cifar_download_to_jpg/master/sample_output.png)
 
--- main.py
+++ main.py
@@ -1,88 +1,56 @@
 import os
 import cPickle
-import random
 import numpy as np
 from scipy.misc import imsave
 
 
 # define the ration between train and val data. Images will be stored into corresponding folders
-trainVsVal = 0.7
+TRAIN_VAL_RATIO = 0.7
 
-# defines the object classes contained in the dataset
-labelNames = [
-    'airplane',
-    'automobile',
-    'bird',
-    'cat',
-    'deer',
-    'dog',
-    'frog',
-    'horse',
-    'ship',
-    'truck']
 
-# extract all images in one cifar batch
-def load_batch(path, filename):
+def load_batch(filename):
+    """Extracts all images and their according class label from given cifar batch
 
-    batch = open(path + filename, 'rb')
-    dict = cPickle.load(batch)
-    images = dict['data']
+    Arguments:
+        filename {[string]} -- [path to the cifar batch to use]
+
+    Returns:
+        [tuple] -- [np.array of images and np.array of according labels]
+    """
+
+    batch = open(filename, 'rb')
+    data_dict = cPickle.load(batch)
+    images = data_dict['data']
     images = np.reshape(images, (10000, 3, 32, 32))
-    labels = dict['labels']
-    imageArray = np.array(images)
-    labelArray = np.array(labels)
+    labels = data_dict['labels']
 
-    return imageArray, labelArray
+    return (np.array(images), np.array(labels))
+
+
+def make_dir_if_not_exists(dirpath):
+    """creates a directory on disk if it does not already exist
+
+    Arguments:
+        dirpath {[string]} -- [path to the dir that shall be created]
+    """
+    if not os.path.exists(dirpath):
+        os.makedirs(dirpath)
 
 
 def main():
+
+    # define the object classes contained in the dataset
+    category_names = ['airplane', 'automobile', 'bird', 'cat',
+                      'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
     # create output folders if they do not exist
-    if not os.path.exists("./cifar10"):
-        os.makedirs("./cifar10")
-    if not os.path.exists("./cifar10/train"):
-        os.makedirs("./cifar10/train")
-    if not os.path.exists("./cifar10/val"):
-        os.makedirs("./cifar10/val")
-    if not os.path.exists("./cifar10/train/airplane"):
-        os.makedirs("./cifar10/train/airplane")
-    if not os.path.exists("./cifar10/train/automobile"):
-        os.makedirs("./cifar10/train/automobile")
-    if not os.path.exists("./cifar10/train/bird"):
-        os.makedirs("./cifar10/train/bird")
-    if not os.path.exists("./cifar10/train/cat"):
-        os.makedirs("./cifar10/train/cat")
-    if not os.path.exists("./cifar10/train/deer"):
-        os.makedirs("./cifar10/train/deer")
-    if not os.path.exists("./cifar10/train/dog"):
-        os.makedirs("./cifar10/train/dog")
-    if not os.path.exists("./cifar10/train/frog"):
-        os.makedirs("./cifar10/train/frog")
-    if not os.path.exists("./cifar10/train/horse"):
-        os.makedirs("./cifar10/train/horse")
-    if not os.path.exists("./cifar10/train/ship"):
-        os.makedirs("./cifar10/train/ship")
-    if not os.path.exists("./cifar10/train/truck"):
-        os.makedirs("./cifar10/train/truck")
-    if not os.path.exists("./cifar10/val/airplane"):
-        os.makedirs("./cifar10/val/airplane")
-    if not os.path.exists("./cifar10/val/automobile"):
-        os.makedirs("./cifar10/val/automobile")
-    if not os.path.exists("./cifar10/val/bird"):
-        os.makedirs("./cifar10/val/bird")
-    if not os.path.exists("./cifar10/val/cat"):
-        os.makedirs("./cifar10/val/cat")
-    if not os.path.exists("./cifar10/val/deer"):
-        os.makedirs("./cifar10/val/deer")
-    if not os.path.exists("./cifar10/val/dog"):
-        os.makedirs("./cifar10/val/dog")
-    if not os.path.exists("./cifar10/val/frog"):
-        os.makedirs("./cifar10/val/frog")
-    if not os.path.exists("./cifar10/val/horse"):
-        os.makedirs("./cifar10/val/horse")
-    if not os.path.exists("./cifar10/val/ship"):
-        os.makedirs("./cifar10/val/ship")
-    if not os.path.exists("./cifar10/val/truck"):
-        os.makedirs("./cifar10/val/truck")
+
+    for dirname in ('cifar10', 'cifar10/train', 'cifar10/val'):
+        make_dir_if_not_exists(dirname)
+    for dir_level_1 in ('train', 'val'):
+        for dir_level_2 in category_names:
+            make_dir_if_not_exists(os.path.join(
+                'cifar10', dir_level_1, dir_level_2))
 
     # download the dataset
     os.system("wget -c https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz")
@@ -92,24 +60,24 @@ def main():
     os.system("rm cifar-10-python.tar.gz")
 
     # loop over all the downloaded cifar data batches
-    counter = 0
     for filename in os.listdir("./cifar-10-batches-py"):
         if filename.startswith("data_batch"):
-            images, labels = load_batch("./cifar-10-batches-py" + "/", filename)
+            images, labels = load_batch(os.path.join(
+                "./cifar-10-batches-py", filename))
 
-	    # save data to the folders
-            for image, label in zip(images, labels):
-                if counter % 10 >= trainVsVal*10:
-                    trainOrValDir = 'val/'
+            # unpack and save data to the folders
+            for index, (image, label) in enumerate(zip(images, labels)):
+                if index % 10 >= TRAIN_VAL_RATIO*10:
+                    train_or_val = 'val/'
                 else:
-                    trainOrValDir = 'train/'
-                filenameImage = str(counter) + ".jpg"
-                counter = counter + 1
+                    train_or_val = 'train/'
                 image = image.transpose(1, 2, 0)
-                imsave("./cifar10/" + trainOrValDir + labelNames[label] +"/"+ filenameImage, image)
+                imsave(os.path.join("./cifar10", train_or_val,
+                                    category_names[label], str(index) + ".jpg"), image)
 
     # cleanup the cifar-10-batches-py folder
     os.system("rm -r cifar-10-batches-py")
+
 
 if __name__ == "__main__":
     main()
